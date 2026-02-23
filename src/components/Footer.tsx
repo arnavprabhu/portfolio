aadac51e64c1345bc6a044a6a5a9fda361d7416c
Arnav Prabhu <arnavprabhu27@gmail.com>
2026-02-22 22:38:16 -0600

    Fix: scroll back to top

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,9 +17,9 @@ export default function Footer() {
       <Image
         src="/aplogo.png"
         alt="AP"
-        width={24}
-        height={24}
-        className="h-5 w-5 shrink-0 opacity-80 sm:h-6 sm:w-6"
+        width={40}
+        height={40}
+        className="h-8 w-8 shrink-0 opacity-80 sm:h-10 sm:w-10"
       />
       <p className="text-sm text-[#86868B]">
         © {year} Arnav Prabhu. All rights reserved.
